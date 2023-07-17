05b01f0304aa42e3f26aa97f7a144c631b3e434f
L9029 <angeloca03@gmail.com>
2023-07-16 23:32:26 -0400

    Creating the algorithm for the main function

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from modules import Stack
 
-s = Stack()
+def my_stack_function(arr):
+    p = Stack()
+    p2 = Stack()
+    
+    for i in arr:
+        p.push(i)
+    
+    while p.isEmpty() == False:
+        act = p.pop()
+        if p2.isEmpty() == False:
+            c = 0
+            while c != p2.size():
+                if p2.top() < act:
+                    s = p2.pop()
+                    p.push(s)
+                else:
+                    p2.push(act)
+                    c = p2.size()
+        else:
+            p2.push(act)
+    
+    return p2.items
 
-def my_stack_funtion(arr):
-    pass
-
-my_stack_funtion([1, 0, -2, -33, 10])
+result = my_stack_function([2, 5, 1, 7, 0, 4, 7, 8])
+print(result)
